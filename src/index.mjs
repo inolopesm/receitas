8956a6c41c96daaa15c327344114dc05f2aee055
Matheus Inocencio Lopes <inolopesm@gmail.com>
2021-08-14 17:23:31 -0300

    move arquivos estáticos para utilizar views

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,13 @@
 import express from 'express'
+import recipes from './data/recipes.json'
 
 const app = express()
 
-app.use(express.static('static'))
+app.set('view engine', 'ejs')
+app.set('views', './src/views')
+
+app.get('/', (request, response) => {
+    return response.render('index', { recipes })
+})
 
 app.listen(3000)
